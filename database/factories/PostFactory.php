91a9a283b7620a4b99ab909670c972f2e0ed8543
arkarWebDev <arkar84848484@gmail.com>
2022-10-23 13:59:51 +0630

    after migration Stage-3

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence();
+        $description = $this->faker->realText(2000);
+        return [ 
+            "title" => $title,
+            "slug" => Str::slug($title),
+            "description" => $description,
+            "excerpt" => Str::words($description,50,),
+            "user_id" => User::inRandomOrder()->first()->id,
+            "category_id" => Category::inRandomOrder()->first()->id
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +18,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Arkar',
+            'email' => 'admin@gmail.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(9)->create();
+
+        $categories = ["Teach","Health","Food & Travel","International","Breaking"];
+        foreach ($categories as $category) {
+            Category::factory()->create([
+                "title" => $category,
+                "slug" => Str::slug($category),
+                "user_id" => User::inRandomOrder()->first()->id
+            ]);
+        }
+
+        Post::factory(200)->create();
     }
 }
